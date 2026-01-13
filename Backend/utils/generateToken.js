d5fc7ecf8ce9ae157b7266acd033570da9b2314f
Sartaj Alam <Sartaj9806@gmail.com>
2026-01-13 10:55:55 +0530

    Fixed mail service and cookies extend time

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -5,7 +5,7 @@ export const generateToken = (userId, res) => {
 
 
     const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-        expiresIn: '10s'
+        expiresIn: '5m'
     })
 
     res.cookie('token', token, {
